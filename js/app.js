cdc78a912abecf00011bb8d9b4d5ef3b696f228d
shiwantha viraj <shiwanvir@gmail.com>
2024-01-10 11:30:34 -0500

    final comments
    make some comments for missing areas

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
+//this will get trigger after html loads, not wating to load external libraries to laod
 document.addEventListener('DOMContentLoaded', function () {
+    //carate const variable for base url
     const baseUrl = 'http://localhost:8080/';
+    //from elements
     var noteForm = document.getElementById('note_form');
     var submitButton = document.getElementById('submit_button');
     var cancelButton = document.getElementById('cancel_button');
+    //table elements
     var notesTable = document.getElementById('notes-table');
     var emptyTableMessage = document.getElementById('empty-table-message');
     var notesTableBody = document.querySelector('#notes-table tbody')
 
-
+    //getting fired when shubmit button hits
     noteForm.addEventListener('submit', function (event) {
-        //prevent default form submission
+        //prevent default form submission(page relod)
         event.preventDefault()
-        //get form attribute values
+        //get form filed attribute values
         var noteId = document.getElementById('id').value;
         var title = document.getElementById('title').value;
         var description = document.getElementById('description').value;
-
+        //create note object
         var noteObject = {
             'id': noteId,
             'title': title,
@@ -32,26 +36,31 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
     })
-
+    //cancel button event listner
     cancelButton.addEventListener('click', function (event) {
         noteForm.reset()
         submitButton.innerText = 'Save';
     });
 
+    //load note list 
     function loadNoteTable() {
         fetch(baseUrl + 'notes')
             .then(response => {
+                //if request fails
                 if (!response.ok) {
-                    //throw new Error(`HTTP error! Status: ${response.status}`);
+                    console.log(`HTTP error! Status: ${response.status}`);
                 }
                 return response.json();
             })
             .then(data => {
-                //clear curret table
+
                 notesTable.style.display = data.data !== null ? 'inline-block' : 'none';
                 emptyTableMessage.style.display = data.data !== null ? 'none' : 'block';
+                //clear curret table
                 notesTableBody.innerHTML = '';
+                //loop through data object
                 data.data.forEach(function (note) {
+                    //create new table raw for each object
                     var new_row = notesTableBody.insertRow();
                     //pupulate raw data
                     new_row.insertCell(0).textContent = note.id;
@@ -62,35 +71,41 @@ document.addEventListener('DOMContentLoaded', function () {
                     var actionsCell = new_row.insertCell(3);
                     var updateButton = document.createElement('button');
                     updateButton.textContent = 'Edit';
+                    //adding class for edit button dynamically
                     updateButton.classList.add('edit-button');
                     updateButton.addEventListener('click', function () {
                         // Handle update button click
                         handleEditButton(note);
                     });
+                    //appending edit button into cell
                     actionsCell.appendChild(updateButton);
 
                     var deleteButton = document.createElement('button');
                     deleteButton.textContent = 'Delete';
+                    //adding class for edit button dynamically
                     deleteButton.classList.add('delete-button');
                     deleteButton.addEventListener('click', function () {
                         // Handle delete button click
                         handleDeleteButton(note);
                     });
+                    //appending delete button into cell
                     actionsCell.appendChild(deleteButton);
 
 
                 })
             })
             .catch(error => {
+                //show empty table message
                 emptyTableMessage.style.display = 'block';
                 console.log('Error fetching notes:', error);
 
             });
     }
 
-
+    //calling load note list function
     loadNoteTable();
 
+    //edit note
     function handleEditButton(note) {
         // Populate the form with the selected note for updating
         document.getElementById('id').value = note.id;
@@ -99,7 +114,7 @@ document.addEventListener('DOMContentLoaded', function () {
         submitButton.innerText = 'Update';
 
     }
-
+    //delete note
     function handleDeleteButton(note) {
         var confirmation = confirm("Are you sure you want to delete note!");
         if (confirmation) {
@@ -107,7 +122,7 @@ document.addEventListener('DOMContentLoaded', function () {
             loadNoteTable()
         }
     }
-
+    //save note
     function saveNote(noteObject) {
         fetch(baseUrl + 'notes', {
             method: 'POST',
@@ -118,6 +133,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         }).then(response => response.json())
             .then(data => {
+                //if sucessfully saved
                 alert("Note Sucessfully Saved");
                 noteForm.reset();
                 loadNoteTable();
@@ -128,7 +144,7 @@ document.addEventListener('DOMContentLoaded', function () {
             });
 
     }
-
+    //update note
     function updateNote(noteObject) {
         fetch(baseUrl + 'update', {
             method: 'PUT',
@@ -139,6 +155,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         }).then(response => response.json())
             .then(data => {
+                //if sucessfully updated
                 alert("Note Sucessfully Updated");
                 noteForm.reset();
                 document.getElementById('id').value = "";
@@ -151,7 +168,7 @@ document.addEventListener('DOMContentLoaded', function () {
             });
 
     }
-
+    //delete note
     function deleteNote(noteObject) {
         fetch(`${baseUrl}delete/${noteObject.id}`, {
             method: 'DELETE',
@@ -167,7 +184,7 @@ document.addEventListener('DOMContentLoaded', function () {
             return response.text();
         })
             .then(data => {
-
+                //if sucessfully deleted
                 alert(JSON.parse(data).message);
                 loadNoteTable();
             })
@@ -176,7 +193,7 @@ document.addEventListener('DOMContentLoaded', function () {
             });
 
     }
-
+    //validate form fields
     function validateformFileds() {
         var title = document.getElementById('title').value.trim();
         var description = document.getElementById('description').value.trim();
